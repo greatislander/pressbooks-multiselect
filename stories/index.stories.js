29e7f5ce739683b88cedead4e554059330862fab
Ned Zimmerman <ned@bight.dev>
2024-03-12 21:18:48 -0300

    feat: add support for maximum items (resolves #116)

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import '../pressbooks-multiselect.js';
 
 export default {
@@ -6,15 +7,18 @@ export default {
   component: 'pressbooks-multiselect',
 };
 
-function Template({ slot, styles }) {
+function Template({ slot, max, styles }) {
   return html`<style>
       body { font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; } ${styles}
     </style>
-    <pressbooks-multiselect> ${slot} </pressbooks-multiselect> `;
+    <pressbooks-multiselect max=${ifDefined(max)}>
+      ${slot}
+    </pressbooks-multiselect> `;
 }
 
 export const Regular = Template.bind({});
 Regular.args = {
+  max: 0,
   slot: html`<label for="dwarves">Dwarves</label>
     <select
       id="dwarves"
@@ -39,6 +43,7 @@ Regular.args = {
 };
 
 export const MaxWidth = Template.bind({});
+
 MaxWidth.args = {
   styles: `:root {
     --pb-combo-container-max-width: 40rem;
@@ -183,6 +188,33 @@ Disabled.args = {
       id="dwarves"
       name="dwarves[]"
       disabled
+      multiple
+      aria-describedby="dwarves-hint"
+    >
+      <option value="thorin">Thorin</option>
+      <option value="dwalin">Dwalin</option>
+      <option value="balin">Balin</option>
+      <option value="bifur">Bifur</option>
+      <option value="bofur">Bofur</option>
+      <option value="bombur">Bombur</option>
+      <option value="fili">Fili</option>
+      <option value="kili">Kili</option>
+      <option value="oin">Oin</option>
+      <option value="gloin">Gloin</option>
+      <option value="nori">Nori</option>
+      <option value="dori">Dori</option>
+      <option value="ori">Ori</option>
+    </select>`,
+};
+
+export const MaxItems = Template.bind({});
+MaxItems.args = {
+  max: 3,
+  slot: html`<label for="dwarves">Dwarves</label>
+    <p id="dwarves-hint">Type to choose some dwarves (maximum 3).</p>
+    <select
+      id="dwarves"
+      name="dwarves[]"
       multiple
       aria-describedby="dwarves-hint"
     >
